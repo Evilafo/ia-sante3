248270195b9d0dc3387eec62944dfae17cf99591
Evilafo Emmanuel <36644643+Evilafo@users.noreply.github.com>
2024-05-31 20:44:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ def generate_llama2_response(prompt_input):
 
 # User-provided prompt
 #if prompt := st.chat_input(disabled=not replicate_api):
-if prompt := st.chat_input(disabled=not replicate_api):
+if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.write(prompt)
